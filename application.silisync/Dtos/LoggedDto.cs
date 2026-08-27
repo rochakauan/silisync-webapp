40e1ab7f03b86bc942563ed1c13af6082ae8b0ec
Kauan Rocha <kauanrochapacc@gmail.com>
2026-08-26 23:07:05 -0300

    feat: LoggedDto returned in LoginAsync() and field sanitization in AuthUtils.cs

---
+++ application.silisync/Dtos/LoggedDto.cs
@@ -0,0 +1,9 @@
+﻿namespace application.silisync.Dtos;
+
+public class LoginDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    
+    public string AccessToken { get; set; } = string.Empty;
+}
---
+++ application.silisync/Utils/AuthUtils.cs
@@ -0,0 +1,6 @@
+﻿namespace application.silisync.Utils;
+
+public class AuthUtils
+{
+    
+}
